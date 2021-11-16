bc4a893b41240515c852705777a82123bf6ebf00
Chirag Singla <chirag.singla.pi@gmail.com>
2021-11-16 23:24:27 +0530

    Clean up code

--- components/anime/Banner.js
+++ components/anime/Banner.js
@@ -3,22 +3,25 @@ import { useRouter } from 'next/router'
 import useMediaQuery from '../../hooks/useMediaQuery'
 import { PlayIcon } from "@heroicons/react/solid";
 import { ClockIcon, ThumbUpIcon } from "@heroicons/react/outline";
+import Icon from '../Icon'
 
 function Banner({ anime }) {
-    const isMedium = useMediaQuery('(min-width: 768px)')
-    const isLarge = useMediaQuery('(min-width: 1024px)')
-    const router = useRouter()
+  const isMedium = useMediaQuery('(min-width: 768px)')
+  const isLarge = useMediaQuery('(min-width: 1024px)')
+  const router = useRouter()
 
-    let description = ""
+  let description = ""
 
-    if (isLarge) {
-        description = anime.description.substr(0, 320)
-    } else if (isMedium) {
-        description = anime.description.substr(0, 300)
-    }
+  if (isLarge) {
+      description = anime.description.substr(0, 320)
+  } else if (isMedium) {
+      description = anime.description.substr(0, 300)
+  }
 
-    return (
+  return (
     <div className='relative h-[200px] sm:h-[250px] md:h-[300px] lg:h-[350px] xl:h-[400px] 2xl:h-[450px]'>
+
+      {/* The image behind the banner */}
       {anime.bannerImage ?
         <Image
           priority
@@ -30,37 +33,28 @@ function Banner({ anime }) {
         : null
       }
 
-      <div className='text-white absolute ml-4 sm:ml-8 mt-4 sm:mt-6 lg:mt-10 xl:mt-12 2xl:mt-14 space-y-2'>
+      <div className='text-white absolute ml-4 sm:ml-8 mt-4 sm:mt-6 lg:mt-8 xl:mt-10 2xl:mt-12 space-y-2 md:space-y-3'>
         <p className='font-extrabold text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl'>{anime.title.english}</p>
         <p className='text-gray-300 font-normal text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl'>{anime.title.romaji}</p>
 
         <div className='flex space-x-2'>
-            <div className='flex items-center space-x-1'>
-                <PlayIcon className='w-4 h-4' />
-                <p className='text-xs sm:text-sm xl:text-base'>{anime.format}</p>
-            </div>
-            <div className='flex items-center space-x-1'>
-                <ClockIcon className='w-4 h-4' />
-                <p className='text-xs sm:text-sm xl:text-base'>{anime.duration} Min/Ep</p>
-            </div>
-            <div className='flex items-center space-x-1'>
-                <ThumbUpIcon className='w-4 h-4' />
-                <p className='text-xs sm:text-sm xl:text-base'>{anime.meanScore}%</p>
-            </div>
+          <Icon icon={PlayIcon} text={anime.format} />
+          <Icon icon={ClockIcon} text={`${anime.duration} Min/Ep`} />
+          <Icon icon={ThumbUpIcon} text={`${anime.meanScore}%`} />
         </div>
 
         <div className='grid grid-cols-4 gap-x-1 sm:gap-x-3 md:gap-x-4 gap-y-1 mr-2 md:flex'>
-            {anime.genres.map(genre => <p className='p-1 rounded text-xs sm:text-sm bg-white text-black'>{genre}</p>)}
+          {anime.genres.map(genre => <p className='p-1 rounded text-xs sm:text-sm bg-white text-black'>{genre}</p>)}
         </div>
 
         {/* todo, find a safer way */}
         {description ?
-            <p className='max-w-3xl' dangerouslySetInnerHTML={{ __html: `${description}...` }}></p>
-            : null
+          <p className='max-w-3xl' dangerouslySetInnerHTML={{ __html: `${description}...` }}></p>
+          : null
         }
 
         <button
-          className='flex items-center text-xs sm:text-sm md:text-base rounded-lg px-2 py-1 bg-[#C3073F] active:scale-90 transform transition duration-300 ease-in'
+          className='flex items-center px-2 py-1 rounded-lg text-xs sm:text-sm md:text-base bg-[#C3073F] text-white active:scale-90 transform transition duration-300 ease-in'
           onClick={()=>router.push(`/anime/${anime.id}`)}
         >
           <PlayIcon className='w-5' />
--- components/anime/Card.js
+++ components/anime/Card.js
@@ -2,41 +2,33 @@ import { useRouter } from "next/router"
 import Image from "next/image"
 import { PlayIcon } from "@heroicons/react/solid";
 import { ClockIcon, ThumbUpIcon } from "@heroicons/react/outline";
+import Icon from '../Icon'
 
 function Card({anime}) {
-    const router = useRouter()
+  const router = useRouter()
 
-    return (
-        <div className='w-52 sm:w-56 p-2 hover:scale-105 transform transition duration-300 ease-out'>
-            <div className='relative w-40 sm:w-52 h-48 sm:h-64 cursor-pointer' onClick={()=>router.push(`/anime/${anime.id}`)}>
-                <Image
-                    src={anime.coverImage.extraLarge}
-                    layout='fill'
-                    objectPosition='center'
-                    className='rounded-md'
-                />
-            </div>
-
-            <div className='h-20'>
-                <span className='text-sm mt-2 text-white'>{anime.title.romaji}</span>
-            </div>
-
-            <div className='flex space-x-2 text-white text-xs mx-auto'>
-                <div className='hidden sm:flex items-center space-x-1'>
-                    <PlayIcon className='w-4 h-4' />
-                    <p>{anime.format}</p>
-                </div>
-                <div className='flex items-center space-x-1'>
-                    <ClockIcon className='w-4 h-4' />
-                    <p>{anime.duration} Min</p>
-                </div>
-                <div className='flex items-center space-x-1'>
-                    <ThumbUpIcon className='w-4 h-4' />
-                    <p>{anime.meanScore}%</p>
-                </div>
-            </div>
-        </div>
-    )
+  return (
+    <div className='w-52 sm:w-56 p-2 hover:scale-105 transform transition duration-300 ease-out'>
+      <div className='relative w-40 sm:w-52 h-48 sm:h-64 cursor-pointer' onClick={()=>router.push(`/anime/${anime.id}`)}>
+        <Image
+        src={anime.coverImage.extraLarge}
+        layout='fill'
+        objectPosition='center'
+        className='rounded-md'
+      />
+      </div>
+    
+      <div className='h-20'>
+        <span className='text-sm mt-2 text-white'>{anime.title.romaji}</span>
+      </div>
+    
+      <div className='flex space-x-2 text-white text-xs mx-auto'>
+        <Icon icon={PlayIcon} text={anime.format} className='hidden sm:flex' />
+        <Icon icon={ClockIcon} text={`${anime.duration} Min`} />
+        <Icon icon={ThumbUpIcon} text={`${anime.meanScore}%`} />
+      </div>
+    </div>
+  )
 }
-
+  
 export default Card
