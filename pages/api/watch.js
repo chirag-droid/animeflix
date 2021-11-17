048f7ef9c1657804f12b791013ca60999e57c52b
Chirag Singla <81347541+chirag-droid@users.noreply.github.com>
2021-11-17 18:30:14 +0530

    Use a better regex

--- pages/api/watch.js
+++ pages/api/watch.js
@@ -29,7 +29,7 @@ export default async function handler(req, res) {
       })
     }
   }
-  
+
   res.status(200).json({
     props: {
       videoLink: await getVideoLink(videoLink, options),
@@ -42,7 +42,7 @@ export default async function handler(req, res) {
 
 const gogoLink = (name) => {
   name = name.split(" ").join("-")
-  name = name.replace(/[:()]/g, "")
+  name = name.replace(/[^\w-]/g, "")
 
   return `https://gogoanime.cm/category/${name}`
 }
