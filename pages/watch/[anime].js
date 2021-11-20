ff8fe8e30deed3030aa1b25123eee460e806fb29
Chirag Singla <chirag.singla.pi@gmail.com>
2021-11-20 18:22:13 +0530

    Use youtube like sizing for video player

--- pages/watch/[anime].js
+++ pages/watch/[anime].js
@@ -14,13 +14,15 @@ function Video({ videoLink, poster }) {
     <>
       <Header />
 
-      {videoLink ?
-        <VideoPlayer src={videoLink} poster={poster} />
-        :
-        <p className='font-semibold text-white mt-4 ml-3 sm:ml-6 text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl'>
-          {errMessage}
-        </p>
-      }
+      <div className='max-w-[800px] mt-4 mx-auto sm:p-4 lg:p-0 lg:ml-4 lg:mx-0 lg:max-w-full lg:w-[65%]'>
+        {videoLink ?
+          <VideoPlayer src={videoLink} poster={poster} />
+          :
+          <p className='font-semibold text-white mt-4 ml-3 sm:ml-6 text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl'>
+            {errMessage}
+          </p>
+        }
+      </div>
     </>
   )
 }
