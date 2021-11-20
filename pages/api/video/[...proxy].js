599fa1b744d46e9aeef597b3fa84891652d31081
Chirag Singla <chirag.singla.pi@gmail.com>
2021-11-21 02:05:08 +0530

    enable browser caching by sending etag and expires header

--- pages/api/video/[...proxy].js
+++ pages/api/video/[...proxy].js
@@ -8,6 +8,17 @@ export default async function handler(req, res) {
     }
   }
 
+  // fetch the data from the url
   const response = await fetch(url, options)
+
+  const setHeader = header => {
+    res.setHeader(header, response.headers.get(header.toLowerCase()))
+  }
+
+  // set etag, and expires header so that the browser caches the video data
+  setHeader("etag")
+  setHeader("expires")
+
+  // send the response data back to the client
   res.send(response.body)
 }
