f61313d21c57d0d22a8d6132ce1d4c7b20e4d64c
Chirag Singla <chirag.singla.pi@gmail.com>
2021-11-21 20:18:30 +0530

    Add previous and next episode controls

--- components/VideoPlayer.js
+++ components/VideoPlayer.js
@@ -1,14 +1,42 @@
-import { Player, DefaultUi, Hls } from '@vime/react';
+import {
+  Player,
+  DefaultUi,
+  Hls,
+  Controls,
+  DefaultControls,
+  PlaybackControl,
+  Control
+} from '@vime/react';
 import '@vime/core/themes/default.css';
+import { ChevronDoubleLeftIcon, ChevronDoubleRightIcon } from '@heroicons/react/solid';
 
-export default function VideoPlayer({ src, poster }) {
+function ControlIcon({ icon, onClick }) {
+  const HeroIcon = icon
   return (
-    <Player key={src}>
-      <Hls version="latest" poster={poster}>
+    <Control onClick={onClick}>
+      <HeroIcon className='text-white w-12 p-1.5' />
+    </Control>
+  )
+}
+
+export default function VideoPlayer({ src, poster, previousCallback, nextCallback }) {
+  return (
+    <Player>
+      <Hls version="latest" poster={poster} key={src}>
         <source src={src} type="application/x-mpegURL" />
       </Hls>
 
-      <DefaultUi />
+      <DefaultUi noControls>
+        <DefaultControls hideOnMouseLeave activeDuration={1500} />
+
+        <Controls fullwidth pin="center" hideOnMouseLeave activeDuration={1500}>
+          <div className="flex space-x-32 items-center mx-auto">
+            <ControlIcon icon={ChevronDoubleLeftIcon} onClick={previousCallback} />
+            <PlaybackControl hideTooltip style={{ '--vm-control-scale': 1.7 }} />            
+            <ControlIcon icon={ChevronDoubleRightIcon} onClick={nextCallback} />
+          </div>
+        </Controls>
+      </DefaultUi>
     </Player>
   );
 }
--- pages/watch/[id].js
+++ pages/watch/[id].js
@@ -6,11 +6,23 @@ import { slugify } from "@utility/utils"
 import { animeInfoFragment } from '@utility/fragments'
 import RecommendationCard from '@components/watch/Card'
 import dynamic from 'next/dynamic'
+import { useRouter } from "next/router"
 
 const VideoPlayer = dynamic(() => import("@components/VideoPlayer"), { ssr: false })
 
 function Video({ videoLink, anime, recommended }) {
+  const router = useRouter()
   progress.finish()
+
+  const { id, episode } = router.query
+
+  const previousEpisode = () => {
+    router.push(`/watch/${id}?episode=${parseInt(episode)-1}`)
+  }
+
+  const nextEpisode = () => {
+    router.push(`/watch/${id}?episode=${parseInt(episode)+1}`)
+  }
 
   return (
     <>
@@ -19,9 +31,14 @@ function Video({ videoLink, anime, recommended }) {
       <div className='lg:flex mt-4 space-x-4'>
         <div className='max-w-[800px] mx-auto sm:p-4 lg:p-0 lg:ml-4 lg:mx-0 lg:max-w-full lg:w-[65%]'>
           {videoLink ?
-            <VideoPlayer src={videoLink} poster={anime.bannerImage} /> :
+            <VideoPlayer
+              src={videoLink}
+              poster={anime.bannerImage}
+              nextCallback={nextEpisode}
+              previousCallback={previousEpisode} 
+            /> :
             <p className='font-semibold text-white mt-4 ml-3 sm:ml-6 text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl'>
-              {errMessage}
+              {"Sorry, the anime video couldn't be found"}
             </p>
           }
 
