ba975c5a91d3e4b5f57801be3d605bec73c7f4d1
futaba <mythhb@yahoo.com>
2022-04-27 11:51:37 -0400

    Fix: slug search

--- utility/gogoanime.js
+++ utility/gogoanime.js
@@ -1,26 +1,28 @@
-import { scrapeMP4 } from "gogoanime-api/lib/anime_parser"
-const cheerio = require("cheerio")
+import { scrapeMP4, scrapeSearch } from "gogoanime-api/lib/anime_parser";
+const cheerio = require("cheerio");
 
 const options = {
   headers: {
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36 Edg/95.0.1020.44'
-  }
-}
+    "user-agent":
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36 Edg/95.0.1020.44",
+  },
+};
 
 const options2 = {
   headers: {
-    'x-requested-with': 'XMLHttpRequest'
-  }
-}
+    "x-requested-with": "XMLHttpRequest",
+  },
+};
 
 async function getAnime(slug, episode) {
-  if (slug == "") return
-    const data = await scrapeMP4({id: `${slug}-episode-${episode}`})
-    return {
-      referrer: data.Referer,
-      videoLink: data.sources?.[0].file
-    }
+  if (slug == "") return;
+  const findAnime = await scrapeSearch({ keyw: slug });
+  const data = await scrapeMP4({ id: `${findAnime[0].animeId}-episode-${episode}` });
+
+  return {
+    referrer: data.Referer,
+    videoLink: data.sources?.[data.sources.length - 1].file,
+  };
 }
 
-
-export default getAnime
+export default getAnime;
