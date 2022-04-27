bb7c450e70e229bdcd116342aeab3b68f3c99d61
futaba <mythhb@yahoo.com>
2022-04-27 11:58:39 -0400

    Feat: Dependencies clean up

--- utility/gogoanime.js
+++ utility/gogoanime.js
@@ -1,19 +1,6 @@
 import { scrapeMP4, scrapeSearch } from "gogoanime-api/lib/anime_parser";
 const cheerio = require("cheerio");
 
-const options = {
-  headers: {
-    "user-agent":
-      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36 Edg/95.0.1020.44",
-  },
-};
-
-const options2 = {
-  headers: {
-    "x-requested-with": "XMLHttpRequest",
-  },
-};
-
 async function getAnime(slug, episode) {
   if (slug == "") return;
   const findAnime = await scrapeSearch({ keyw: slug });
