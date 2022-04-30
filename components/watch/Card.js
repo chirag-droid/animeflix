11083c07bdb012e430e6f1c3df397a451359dad5
Chirag Singla <chirag.singla.pi@gmail.com>
2022-04-30 10:42:46 +0530

    Feat: refactor code and fix visual bugs
    * Feat: refactored some code (#79)
    
    * Add babelrc
    
    * Fix: slug search
    
    * Feat: Select best quality automatically
    
    * Feat: Dependencies clean up
    
    * Feat: Added anime stats to recommeded animes card
    
    * Feat: removed babel;
    
    * Fix: visual bug in recommended animes and add description
    
    Co-authored-by: Marouane <57333995+riimuru@users.noreply.github.com>

--- components/watch/Card.js
+++ components/watch/Card.js
@@ -1,39 +1,49 @@
-import Image from 'next/image'
-import { base64SolidImage } from '@utility/image'
-import { useRouter } from 'next/router'
+import Image from "next/image";
+import { base64SolidImage } from "@utility/image";
+import { useRouter } from "next/router";
+import { PlayIcon } from "@heroicons/react/solid";
+import { ClockIcon, ThumbUpIcon } from "@heroicons/react/outline";
+import Icon from "@components/Icon";
 
 function Card({ anime }) {
-  const router = useRouter()
+  const router = useRouter();
 
   const changeRoute = () => {
     router.push({
       pathname: `/watch/${anime.id}`,
       query: { episode: 1 },
-    })
-  }
+    });
+  };
 
   return (
     <div
-      className='flex space-x-4 ml-2 text-white py-2 h-30 cursor-pointer hover:scale-105 transform transition duration-300 ease-out'
-      onClick={changeRoute}
-    >
-      <div className='relative w-24 h-32'>
+      className="flex space-x-4 ml-2 mr-4 text-white py-2 h-30 cursor-pointer hover:scale-105 transform transition duration-300 ease-out"
+      onClick={changeRoute}>
+      <div className="relative min-w-[6rem] min-h-[8rem] flex-shrink-1">
         <Image
           alt={anime.title.english || anime.title.romaji}
           src={anime.coverImage.large || anime.coverImage.medium || anime.bannerImage}
-          layout='fill'
-          objectPosition='left'
-          className='rounded-md'
+          layout="fill"
+          objectPosition="left"
+          className="rounded-md"
           placeholder="blur"
           blurDataURL={`data:image/svg+xml;base64,${base64SolidImage(anime.coverImage.color)}`}
         />
       </div>
 
-      <p className='w-48'>
-        {anime.title.english || anime.title.romaji}
-      </p>
+      <div className="flex flex-col">
+        <p>{anime.title.english || anime.title.romaji}</p>
+
+        <p className="text-gray-400">{anime.description.slice(0, 70) + "..."}</p>
+
+        <div className="justify-end m-4 flex space-x-2 text-white text-xs">
+          <Icon icon={PlayIcon} text={anime.format} className="hidden sm:flex" />
+          <Icon icon={ClockIcon} text={`${anime.duration} Min`} />
+          <Icon icon={ThumbUpIcon} text={`${anime.meanScore}%`} />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Card
+export default Card;
--- utility/gogoanime.js
+++ utility/gogoanime.js
@@ -1,26 +1,16 @@
-import { scrapeMP4 } from "gogoanime-api/lib/anime_parser"
-const cheerio = require("cheerio")
-
-const options = {
-  headers: {
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36 Edg/95.0.1020.44'
-  }
-}
-
-const options2 = {
-  headers: {
-    'x-requested-with': 'XMLHttpRequest'
-  }
-}
+import { scrapeMP4, scrapeSearch } from "gogoanime-api/lib/anime_parser";
 
 async function getAnime(slug, episode) {
-  if (slug == "") return
-    const data = await scrapeMP4({id: `${slug}-episode-${episode}`})
-    return {
-      referrer: data.Referer,
-      videoLink: data.sources?.[0].file
-    }
+  if (slug == "") return;
+  const findAnime = await scrapeSearch({ keyw: slug });
+  const data = await scrapeMP4({ id: `${findAnime[0].animeId}-episode-${episode}` });
+
+  const bestQuality = data.sources?.[data.sources.length - 1].file;
+
+  return {
+    referrer: data.Referer,
+    videoLink: bestQuality,
+  };
 }
 
-
-export default getAnime
+export default getAnime;
