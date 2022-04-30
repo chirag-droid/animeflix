9c276e417cc969a4a2bb7c0ac5561794583ea691
Chirag Singla <chirag.singla.pi@gmail.com>
2022-04-30 23:07:02 +0530

    fix!: fix internal server on the watch page

--- utility/gogoanime.js
+++ utility/gogoanime.js
@@ -3,6 +3,9 @@ import { scrapeMP4, scrapeSearch } from "gogoanime-api/lib/anime_parser";
 async function getAnime(slug, episode) {
   if (slug == "") return;
   const findAnime = await scrapeSearch({ keyw: slug });
+
+  if (findAnime.length === 0) return;
+
   const data = await scrapeMP4({ id: `${findAnime[0].animeId}-episode-${episode}` });
 
   const bestQuality = data.sources?.[data.sources.length - 1].file;
