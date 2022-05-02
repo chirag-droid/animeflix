489dd030da21715b721aba5953823af2759d96b1
Chirag Singla <chirag.singla.pi@gmail.com>
2022-05-03 03:33:30 +0530

    fix: icon urls in html head and site configs

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,24 +7,24 @@ export default function Document() {
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href="icons/apple-touch-icon.png"
+          href="/icons/apple-touch-icon.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href="icons/favicon-32x32.png"
+          href="/icons/favicon-32x32.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href="icons/favicon-16x16.png"
+          href="/icons/favicon-16x16.png"
         />
         <link rel="manifest" href="/site.webmanifest" />
         <link
           rel="mask-icon"
-          href="icons/safari-pinned-tab.svg"
+          href="/icons/safari-pinned-tab.svg"
           color="#5bbad5"
         />
         <meta name="msapplication-TileColor" content="#da532c" />
