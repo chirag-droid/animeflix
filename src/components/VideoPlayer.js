f8ba65b1a6353724e476751ed3db73758de76c7c
Tom Romeo <Tom.Romeo.Doil@gmail.com>
2022-05-02 21:38:58 +0200

    feat: added playback control shortcuts

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -13,6 +13,7 @@ import {
   Control,
 } from '@vime/react';
 import '@vime/core/themes/default.css';
+import { useRef } from 'react';
 
 function ControlIcon({ icon, onClick }) {
   const HeroIcon = icon;
@@ -29,8 +30,60 @@ export default function VideoPlayer({
   previousCallback,
   nextCallback,
 }) {
+  const videoplayer = useRef(null);
+  
+  document.addEventListener( 'keydown', function( event ) {
+
+    // play-toggle
+    if ( event.key === ' ' || event.key === 'k' ) {
+      if(videoplayer.current.paused) {
+        videoplayer.current.play()
+      }
+      else {
+        videoplayer.current.pause()
+      }
+    }
+
+    // fullscreen
+    if ( event.key === 'f' ) {
+      if(videoplayer.isFullscreenActive) {
+        videoplayer.current.exitFullscreen();
+      }
+      else {
+        videoplayer.current.enterFullscreen();
+      }
+    }
+
+    // volume up
+    if ( event.key === 'ArrowUp' ) {
+      let vol = videoplayer.current.volume;
+      let nextVol = vol + 10;
+      if( nextVol > 100) 
+        nextVol = 100;
+      videoplayer.current.volume = nextVol;
+    }
+
+    // volume down
+    if ( event.key === 'ArrowDown' ) {
+      let vol = videoplayer.current.volume;
+      let nextVol = vol - 10;
+      if( nextVol < 0) 
+        nextVol = 0;
+      videoplayer.current.volume = nextVol;
+    }
+
+    // previous episode
+    if ( event.key === 'ArrowLeft' ) {
+      previousCallback();
+    }
+
+    // next episode
+    if ( event.key === 'ArrowRight' ) {
+      nextCallback();
+    }
+  });
   return (
-    <Player>
+    <Player ref={videoplayer}>
       {src.includes('m3u8') ? (
         <Hls version="latest" poster={poster} key={src}>
           <source data-src={src} type="application/x-mpegURL" />
