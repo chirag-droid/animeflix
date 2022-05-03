aabf9498c513dbed188a802cbb8e2200ca85c980
TomRomeo <60541979+TomRomeo@users.noreply.github.com>
2022-05-03 21:42:26 +0200

    feat: added playback control shortcuts (#96)
    * feat: added playback control shortcuts
    
    * fix: linting
    
    * refactor: using focus() and switch statement

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -1,3 +1,5 @@
+import { useRef, useEffect } from 'react';
+
 import {
   ChevronDoubleLeftIcon,
   ChevronDoubleRightIcon,
@@ -29,8 +31,77 @@ export default function VideoPlayer({
   previousCallback,
   nextCallback,
 }) {
+  const videoplayer = useRef(null);
+  useEffect(() => {
+    document.addEventListener('keydown', (e) => {
+      if (
+        document.activeElement === videoplayer.current ||
+        videoplayer.current?.isFullscreenActive
+      ) {
+        switch (e.key) {
+          // play-toggle
+          case ' ':
+          case 'k':
+            e.preventDefault();
+            if (videoplayer.current.paused) {
+              videoplayer.current.play();
+            } else {
+              videoplayer.current.pause();
+            }
+            break;
+          // fullscreen
+          case 'f':
+            if (e.key === 'f') {
+              if (videoplayer.current?.isFullscreenActive) {
+                videoplayer.current.exitFullscreen().then(() => {
+                  videoplayer.current.focus();
+                });
+              } else {
+                // enterFullscreen throws error if using the native api doesn't work.
+                // After that, it will use the provider
+                // so catching the error is unimportant
+                videoplayer.current.enterFullscreen().catch(() => {
+                  return null;
+                });
+              }
+            }
+            break;
+          // volume up
+          case 'ArrowUp':
+            e.preventDefault();
+            {
+              const vol = videoplayer.current.volume;
+              let nextVol = vol + 10;
+              if (nextVol > 100) nextVol = 100;
+              videoplayer.current.volume = nextVol;
+            }
+            break;
+          // volume down
+          case 'ArrowDown':
+            e.preventDefault();
+            {
+              const vol = videoplayer.current.volume;
+              let nextVol = vol - 10;
+              if (nextVol < 0) nextVol = 0;
+              videoplayer.current.volume = nextVol;
+            }
+            break;
+          // previous episode
+          case 'ArrowLeft':
+            previousCallback();
+            break;
+          // next episode
+          case 'ArrowRight':
+            nextCallback();
+            break;
+          default:
+            break;
+        }
+      }
+    });
+  }, [nextCallback, previousCallback]);
   return (
-    <Player>
+    <Player ref={videoplayer} tabIndex="0" style={{ outline: 'none' }}>
       {src.includes('m3u8') ? (
         <Hls version="latest" poster={poster} key={src}>
           <source data-src={src} type="application/x-mpegURL" />
