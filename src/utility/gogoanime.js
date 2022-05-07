dffa64324c5e34700e95b5772df98f14a08547b9
Marouane <57333995+riimuru@users.noreply.github.com>
2022-05-07 18:18:10 -0400

    feat: fetch kitsu episodes method (#103)

--- src/utility/gogoanime.js
+++ src/utility/gogoanime.js
@@ -31,4 +31,98 @@ async function getAnime(slug, episode) {
   };
 }
 
-export default getAnime;
+/**
+ * slug: string
+ * startDtae: anime media.startDate.year
+ * season: anime media.season
+ * @example 'naruto', startDate: '2019', season: 'WINTER'
+ *
+ */
+async function getKitsuEpisodes(slug, startDate, season) {
+  if (!slug || slug === '') return {};
+
+  const newSlug = slug.replace(/[^0-9a-zA-Z]+/g, ' ');
+
+  const findAnime = await scrapeSearch({ keyw: newSlug });
+
+  if (!findAnime || findAnime.length === 0) return {};
+
+  const episodesList = new Map();
+
+  const anime = findAnime[0];
+
+  const gogoEpisodes = (await scrapeAnimeDetails({ id: anime.animeId }))
+    .episodesList;
+
+  let kitsuEpisodes = await fetch('https://kitsu.io/api/graphql', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      query: `query{searchAnimeByTitle(first:5,title:"${newSlug}"){nodes{id season startDate titles{localized}episodes(first:2000){nodes{number titles{canonical}description thumbnail{original{url}}}}}}}`,
+    }),
+  });
+
+  kitsuEpisodes = await kitsuEpisodes.json();
+
+  if (kitsuEpisodes?.data) {
+    const { nodes } = kitsuEpisodes.data.searchAnimeByTitle;
+    if (nodes) {
+      nodes.forEach((node) => {
+        if (
+          node.season === season &&
+          node.startDate.trim().split('-')[0] === startDate.trim().split('-')[0]
+        ) {
+          const episodes = node.episodes.nodes;
+
+          episodes.forEach((episode) => {
+            if (episode) {
+              const i = episode.number.toString().replace('"', '');
+              let name = null;
+              let description = null;
+              let thumbnail = null;
+
+              if (episode.titles?.canonical)
+                name = episode.titles.canonical.toString().replace('"', '');
+              if (episode.description?.en)
+                description = episode.description.en
+                  .toString()
+                  .replace('"', '')
+                  .replace('\\n', '\n');
+              if (episode.thumbnail)
+                thumbnail = episode.thumbnail.original.url
+                  .toString()
+                  .replace('"', '');
+
+              episodesList.set(i, {
+                episodeNum: episode.number.toString().replace('"', ''),
+                title: name,
+                description,
+                thumbnail,
+              });
+            }
+          });
+        }
+      });
+    }
+  }
+
+  const newEpisodeList = [];
+
+  if (episodesList.size !== 0 && gogoEpisodes.length !== 0) {
+    gogoEpisodes.reverse();
+    gogoEpisodes.forEach((gogoEp, i) => {
+      const j = (i + 1).toString();
+      newEpisodeList.push({
+        episodeId: gogoEp.episodeId,
+        episodeTitle: episodesList.get(j).title,
+        episodeThumbnail: episodesList.get(j).thumbnail,
+        episodeNum: gogoEp.episodeNum,
+        episodeDescription: episodesList.get(j).description,
+      });
+    });
+  }
+
+  return newEpisodeList;
+}
+
+export default { getAnime, getKitsuEpisodes };
