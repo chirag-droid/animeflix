8765fab9cb7a81ee7e155122abae4cbed0da505b
Chirag Singla <chirag.singla.pi@gmail.com>
2022-05-21 12:52:26 +0530

    fix: remove meta tags from _document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -26,8 +26,6 @@ const Document = () => (
         href="/icons/safari-pinned-tab.svg"
         color="#5bbad5"
       />
-      <meta name="msapplication-TileColor" content="#da532c" />
-      <meta name="theme-color" content="#ffffff" />
     </Head>
     <body>
       <Main />
