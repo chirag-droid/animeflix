47e4197e6d9edac094795991affa2a4127269f9b
Chirag Singla <chirag.singla.pi@gmail.com>
2022-05-27 21:28:25 +0530

    chore: disable pwa in development mode

--- next.config.js
+++ next.config.js
@@ -1,13 +1,22 @@
 // @ts-check
 const withPwa = require('next-pwa');
 
-module.exports = withPwa({
+/**
+ * @type {import('next').NextConfig}
+ * */
+const nextConfig = {
   images: {
     domains: ['s4.anilist.co', 'media.kitsu.io'],
     disableStaticImages: true,
   },
   poweredByHeader: false,
+};
+
+module.exports = withPwa({
+  ...nextConfig,
   pwa: {
     dest: 'public',
+    // disable pwa when not in production
+    disable: process.env.NODE_ENV !== 'production',
   },
 });
