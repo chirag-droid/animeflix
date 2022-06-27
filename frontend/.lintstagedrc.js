16f4b6668d6ad1ad39deaaa73a9b108871973f30
chirag-droid <chirag.singla.pi@gmail.com>
2022-06-27 06:03:57 +0530

    feat: setup turbo monorepo

--- frontend/.lintstagedrc.js
+++ frontend/.lintstagedrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  '{codegen.yml,*.gql,**/generated/*.ts}': () => 'npm run generate',
+  '{codegen.yml,*.gql,**/generated/*.ts}': () => 'yarn generate',
   '*.{js,jsx,ts,tsx}': ['eslint --fix', 'eslint'],
   '*.json': ['prettier --write'],
 };
